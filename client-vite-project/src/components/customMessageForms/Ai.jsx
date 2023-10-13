e627204c81434e411d9b45203c403d59a8fdc88b
Yuvika Khurana <97856555+yuvikakhurana@users.noreply.github.com>
2023-10-13 03:54:55 -0400

    Add files via upload

---
+++ client-vite-project/src/components/customMessageForms/Ai.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import MessageFormUI from "./MessageFormUI";
+import { usePostAiTextMutation } from "@/state/api";
+
+const Ai = ({ props, activeChat }) => {
+  const [message, setMessage] = useState("");
+  const [attachment, setAttachment] = useState("");
+
+  //If this was a query - we would be destructuring an object and getting data
+  //Since we are using a mutation, we are destructuring an array and getting a function - TRIGGER  - which will trigger the actual API call
+  const [trigger] = usePostAiTextMutation();
+
+  const handleChange = (e) => setMessage(e.target.value);
+
+  const handleSubmit = async () => {
+    const date = new Date()
+      .toISOString()
+      .replace("T", " ")
+      .replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
+    const at = attachment ? [{ blob: attachment, file: attachment.name }] : [];
+    const form = {
+      attachments: at,
+      created: date,
+      sender_username: props.username,
+      text: message,
+      activeChatId: activeChat.id,
+    };
+
+    //going to submit message
+    props.onSubmit(form);
+    //at the same time going to trigger API call
+    //OPENAI will handle...
+    trigger(form);
+
+    if (message == "Bye") {
+      setTimeout(() => {
+        window.location.assign("https://localhost:3000/");
+      }, 9000);
+    }
+
+    setMessage("");
+    setAttachment("");
+  };
+
+  return (
+    <MessageFormUI
+      setAttachment={setAttachment}
+      message={message}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  );
+};
+
+export default Ai;
